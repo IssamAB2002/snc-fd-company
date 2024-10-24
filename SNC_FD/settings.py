39cbff7cd10cd2199895fbdade3964c0d0eeea5a
Issam <abbasissam98@gmail.com>
2024-10-24 19:20:55 +0100

    change db, adjust requirements

--- SNC_FD/settings.py
+++ SNC_FD/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +76,7 @@ WSGI_APPLICATION = 'SNC_FD.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
 }
 
 
